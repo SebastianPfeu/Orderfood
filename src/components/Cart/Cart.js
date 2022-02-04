4bc8fa8692f3c387a5599ae6e0c40818ce87b8d7
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-02-04 11:33:29 +0100

    Added Modal component

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,3 +1,4 @@
+import Modal from '../UI/Modal';
 import styles from './Cart.module.css';
 
 const Cart = (props) => {
@@ -6,17 +7,17 @@ const Cart = (props) => {
   ].map((item) => <li>{item.name}</li>)}</ul>;
 
   return (
-    <div>
+    <Modal onClose={props.onClose}>
       {cartItems}
       <div className={styles.total}>
         <span>Total Amount</span>
         <span>35.62</span>
       </div>
       <div className={styles.actions}>
-        <button className={styles['button--alt']}>Close</button>
+        <button className={styles['button--alt']} onClick={props.onClose}>Close</button>
         <button className={styles.button}>Order</button>
       </div>
-    </div>
+    </Modal>
   );
 };
 
