89d28f3f99780673a0a90cecd964dc95fcb0dd9f
Sebastian Pfeufer <sebastianpfeufer@icloud.com>
2022-02-06 14:40:41 +0100

    Added logic to CartProvider

--- src/components/Layout/HeaderCartButton.js
+++ src/components/Layout/HeaderCartButton.js
@@ -1,14 +1,22 @@
+import { useContext } from 'react';
 import CartIcon from '../Cart/CartIcon';
+import CartContext from '../../store/cart-context.js';
 import styles from './HeaderCartButton.module.css';
 
 const HeaderCartButton = (props) => {
+  const cartCtx = useContext(CartContext);
+
+  const numberOfCartItems = cartCtx.items.reduce((curNumber, item) => {
+    return curNumber + item.amount;
+  }, 0);
+
   return (
     <button className={styles.button} onClick={props.onClick} >
       <span className={styles.icon}>
         <CartIcon />
       </span>
       <span>Your cart</span>
-      <span className={styles.badge}>3</span>
+      <span className={styles.badge}>{numberOfCartItems}</span>
     </button>
   );
 };
